2708c4aa5d259efcb06f828f489e7a422bd6556d
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2025-02-14 14:53:39 -0500

    Update app.py
    v7

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 
 
 green = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/CategoryTotalsv1.csv")
-roastlog = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/BCMCashflowTimelinev6.csv")
+roastlog = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/BCMCashflowTimelinev7.csv")
 
 #DATA CLEANING
 
