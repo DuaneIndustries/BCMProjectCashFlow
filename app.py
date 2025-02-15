410e2b662a53feae0899cd19b5fa16eaef7081dd
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2025-02-15 09:30:29 -0500

    Update app.py
    v12

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
 green = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/CategoryTotalsv1.csv")
-roastlog = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/BCMCashflowTimelinev11.csv")
+roastlog = pd.read_csv("https://raw.githubusercontent.com/DuaneIndustries/BCMProjectCashFlow/refs/heads/main/BCMCashflowTimelinev12.csv")
 
 #DATA CLEANING
 
