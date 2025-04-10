a3127a52bb0e00609409efad5df9727e09e2ada7
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2025-04-10 17:15:13 -0400

    Update app.py
    starting balance adj

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ app.layout = dbc.Container([
 def update_graph(cat_slctd):
 
     dff = roastlog[roastlog['Vendor'].isin(cat_slctd)]
-    starting_balance = 313154.79 
+    starting_balance = 299573.21
     dff = dff.sort_values(by='Start Date')
     dff['Cumulative Balance'] = dff['Amount'].cumsum() + starting_balance
 
